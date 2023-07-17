8fa64718bd3cbbab9c8a3cf3f5072fb9042d43e3
Yujin <jin37091289@gmail.com>
2023-07-17 10:00:00 +0900

    FEAT :: 1172.c

---
+++ CodeUp/1172.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//
+//
+//int main() {
+//
+//    int num1, num2, num3, temp;
+//    scanf("%d %d %d", &num1, &num2, &num3);
+//
+//    if (num1 >= num2) {
+//        temp = num2;
+//        num2 = num1;
+//        num1 = temp;
+//    }
+//    if (num2 >= num3) {
+//        temp = num3;
+//        num3 = num2;
+//        num2 = temp;
+//    }
+//    if (num1 >= num2) {
+//        temp = num2;
+//        num2 = num1;
+//        num1 = temp;
+//    }
+//
+//    printf("%d %d %d", num1, num2, num3);
+//
+//    return 0;
+//}
+//
