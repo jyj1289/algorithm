705c6c1306439515eae47cb5866439e1e6277182
Yujin <jin37091289@gmail.com>
2023-08-13 10:00:00 +0900

    FEAT :: 1602.c

---
+++ CodeUp/1602.c
@@ -0,0 +1,15 @@
+//#include<stdio.h>
+//double ABS(double value) {
+//	if (value >= 0)
+//		return value;
+//	else
+//		return -value;
+//}
+//int main() {
+//
+//	double value;
+//	scanf("%lf", &value);
+//
+//	printf("%.10g", ABS(value));
+//	return 0;
+//}
