db5f12f4c1980e280d3d4d1e0a354889036feab3
Yujin <jin37091289@gmail.com>
2023-08-14 10:00:00 +0900

    FEAT :: 1602.c

---
+++ CodeUp/1610.c
@@ -0,0 +1,33 @@
+//#include "stdio.h"
+//#include "stdlib.h"
+//#include "string.h"
+//
+//
+//char* mysubstr(char* str, int start, int count)
+//
+//{
+//    char* c = (char*)malloc(count);
+//    memset(c, 0, count);
+//
+//    int cnt = 0;
+//    for (int i = start; i < count + start; i++) {
+//        c[cnt] = str[i];
+//        cnt++;
+//    }
+//    return c;
+//
+//}
+//
+//int main() {
+//    char str[100];
+//    int start;
+//    int count;
+//
+//    scanf("%s", str);
+//    scanf("%d %d", &start, &count);
+//
+//    printf("%s", mysubstr(str, start, count));
+//
+//    return 0;
+//}
+//
